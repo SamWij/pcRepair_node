d73f8eaecde77404d2fa1693fc4f288e046fac58
SamWij <sam.wijesinha@gmail.com>
2017-07-29 17:35:50 +1000

    added contact page and routes

--- routes/contact.js
+++ routes/contact.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const nodemailer = require('nodemailer');
+const router = express.Router();
 
-/* GET home page. */
+/* GET contact page. */
 router.get('/', function(req, res, next) {
   res.render('contact', { title: 'Contact' });
 });
+
+//send email
+router.post('/send', function(req, res, next) {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'sam.wijesinha@gmail.com',
+      pass: 'Lucy!2014'
+    }
+  });
+
+  const mailOptions = {
+    from: '"Sam Wijesinha ?" <sam.wijesinha@gmail.com>',
+    to: 'support@websolutions.com',
+    subject: 'Hello from PCRepair',
+    text: 'You have a submission from... Name: '+req.body.name+' Email: '+req.body.email+' Message: '+req.body.message,
+    html: '<p>You have a submission from...</p> <ul><li>Name: '+req.body.name+'</li><li> Email: '+req.body.email+'</li><li> Message: '+req.body.message+'</li></ul>'
+  }
+
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+        return console.log(error);
+      }
+      console.log('Message Sent: '+ info.response);
+      res.redirect('/');
+    });
+});
+
 
 module.exports = router;
