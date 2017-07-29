e11814e8fd7cb38d5341e0590942de997fd0b5a9
SamWij <sam.wijesinha@gmail.com>
2017-07-29 17:39:15 +1000

    added nodemailer, working email form

--- routes/contact.js
+++ routes/contact.js
@@ -13,13 +13,13 @@ router.post('/send', function(req, res, next) {
     service: 'Gmail',
     auth: {
       user: 'sam.wijesinha@gmail.com',
-      pass: 'Lucy!2014'
+      pass: 'Polly!2014'
     }
   });
 
   const mailOptions = {
     from: '"Sam Wijesinha ?" <sam.wijesinha@gmail.com>',
-    to: 'support@websolutions.com',
+    to: 'sam.wijesinha@gmail.com',
     subject: 'Hello from PCRepair',
     text: 'You have a submission from... Name: '+req.body.name+' Email: '+req.body.email+' Message: '+req.body.message,
     html: '<p>You have a submission from...</p> <ul><li>Name: '+req.body.name+'</li><li> Email: '+req.body.email+'</li><li> Message: '+req.body.message+'</li></ul>'
